85ca9300a826ff092c51898fbc8acc55712f52b9
ProfJessica <jessica.sgarcez@lideres.alicerceedu.com.br>
2021-08-21 12:27:08 -0300

    Primeiro commit

---
+++ src/projetoidades/ProjetoIdades.java
@@ -0,0 +1,19 @@
+package projetoidades;
+
+public class ProjetoIdades {
+
+    public static void main(String[] args) {
+         //Declarei e inicializei variáveis pra cada irmão com o valor das suas IDADES.
+        int Adriano = 19;
+        int Bruna = 21;
+        int Beatriz = 18;
+        int Raul = 20;
+        
+        System.out.println("As idades, do menor ao maior, são: ");
+        System.out.println("Beatriz: " + Beatriz);
+        System.out.println("Adriano: " + Adriano);
+        System.out.println("Raul: " + Raul);
+        System.out.println("Bruna: " + Bruna);
+    }
+    
+}
